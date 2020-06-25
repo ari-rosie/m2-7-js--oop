30f4eb5d739cc2116e8f97f1093fe7efad483752
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-25 14:41:04 -0400

    Exercice-1.4

--- workshop/1-Cat/exercise-1.4.js
+++ workshop/1-Cat/exercise-1.4.js
@@ -11,3 +11,49 @@
 //    It's up to you to decide how quickly these values should change.
 
 // B) Make Boots wait 20 minutes and call then console.log(boots);
+
+class Cat {
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+        this.species = 'cat';
+        this.tiredness = 0;
+        this.hunger = 0;
+        this.loneliness = 0;
+        this.happiness = 0;
+    }
+
+    sleep = (hours) => {
+        this.tiredness -= (hours * 5);
+        this.happiness += (hours * 3);    
+    }
+    eat = (kibbles) => {
+        this.hunger -= (kibbles / 5);
+        this.happiness += (kibbles * 5);
+    }
+    play = (minutes) => {
+        this.loneliness -= (minutes * 3);
+        this.happiness += (minutes * 10);
+    }
+
+    wait = (minutes) => {
+        let thisCat = this;
+        let time = setInterval(function() {
+            thisCat.tiredness += 5;
+            thisCat.hunger += 5;
+            thisCat.loneliness += 2;
+            thisCat.happiness -= 10;        
+        }, 1000);
+        setTimeout(() => {
+            console.log(boots);
+            clearInterval(time);
+        }, minutes * 1000);       
+
+    }
+
+}
+
+let boots = new Cat('Boots', 'siamese');
+console.log(boots);
+boots.wait(5);
+
