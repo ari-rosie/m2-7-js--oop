98a60bc761cc87132081f9aabd890fbdd955bc06
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-25 21:57:15 -0400

    Book-stretch incomplete

---
+++ workshop/3-Books-stretch/app.js
@@ -0,0 +1,89 @@
+class Book {
+    constructor(title, genre, author, isRead, img) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isRead = isRead || false;
+        this.img = img;
+    }
+}
+
+class BookList {
+constructor() {
+    this.books = [];
+    this.lastRead = null;
+    this.currentlyReading = null;
+}
+
+add = (Book) => {
+    this.books.push(Book);
+    if (!this.currentlyReading) this.currentlyReading = Book;
+}
+
+getNumRead = () => {
+    let read = 0;
+    this.books.forEach(book => {
+    if(book.isRead) read++;
+    });
+    return read;
+}
+
+getNumUnread = () => {
+    let unread = 0;
+    this.books.forEach(book => {
+    if(!book.isRead) unread++;
+    });
+    return unread;
+}
+
+finishReading = (title) => {
+    let book = this.books.filter((book) => book.title === title);
+    this.currentlyReading = null;
+    this.lastRead = book;
+}
+
+startReading = (title) => {
+    let book = this.books.filter((book) => book.title === title);
+    this.currentlyReading = book;
+}
+}
+
+const homeLibrary = new BookList();
+
+homeLibrary.add(new Book('The Shining', 'Horror', 'Stephen King','', 'url(imgs/Shiningnovel.jpg)'));
+homeLibrary.add(new Book('American Gods', 'Fiction', 'Neil Gaiman','', 'url(imgs/americangods.jpg)'));
+homeLibrary.add(
+new Book('Eloquent JavaScript', 'Programming', 'Marijn Haverbeke', true, 'url(imgs/eloJS.jpg)')
+);
+homeLibrary.add(new Book('The Eire Affair', 'Fantasy', 'Jasper Fforde','', 'url(imgs/eyreaffair.jpg)'));
+homeLibrary.add(
+new Book('The Revisionists', 'Science-fiction', 'thomas Mullen','', 'url(imgs/revisionists.jpg)')
+);
+
+
+
+//DOM VARIABLE
+const library = document.querySelector('#library');
+const currentBook = document.querySelector('#current');
+const previousBooks = document.querySelector('#previous');
+
+//adding books to HTML library
+homeLibrary.books.forEach(book => {
+    let bookDiv = document.createElement('DIV');
+    bookDiv.setAttribute('class', 'book');
+    library.appendChild(bookDiv);
+    bookDiv.innerText = `${book.title}`;
+    bookDiv.style.backgroundImage = `${book.img}`;
+    console.log(book.img);
+});
+
+//adding book currently reading
+let curr = homeLibrary.currentlyReading;
+
+const addCurrentBook = (book) => {
+    let bookTitle = document.createElement('h2');
+    bookTitle.innerText = `${book.title}`;
+    currentBook.appendChild(bookTitle);
+}
+
+addCurrentBook(curr);
